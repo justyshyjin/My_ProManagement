2daffe3c86b4d376028954c05ac85d2de94ee2aa
justyshyjin <justyshyjin@gmail.com>
2018-09-06 15:35:39 +0530

    Initial files

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pro Manager</title>
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="head">
+        <header>
+            <section class="logo">
+                <span>
+                    <img src="assets/images/logo.png" width="100%" height="100%">
+                </span>
+            </section>
+            <section class="title">
+                <span>
+                    <h3>My Pro Management</h3>
+                </span>
+            </section>
+             <section class="profile">
+                <div class="profileImage">
+
+                </div>
+                <span>
+                    <label>Hi Demo</label>
+                </span>
+            </section>
+        </header>
+    </div>
+</body>
+</html>
